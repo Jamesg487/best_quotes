8a36ad75b7f6923e8e6fac85baa9b2c751e12544
James Green <james.green@freeagent.com>
2025-03-10 12:03:08 +0000

    chapter 7

--- sqlite_test.rb
+++ sqlite_test.rb
@@ -1,14 +1,16 @@
 require "sqlite3"
 require "rulers/sqlite_model"
+require "debug"
 
 class MyTable < Rulers::Model::SQLite; end
 STDERR.puts MyTable.schema.inspect
 
+# debugger
 # Create row
 mt = MyTable.create "title" => "I saw it again!"
-mt["title"] = "I really did!"
+mt.title = "I really did!"
 mt.save!
 
-mt2 = MyTable.find mt["id"]
+mt2 = MyTable.find mt.id
 
-puts "Title: #{mt2["title"]}"
+puts "Title: #{mt2.title}"
